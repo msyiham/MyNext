4172aa1495895d0251266b26c09cb7fa059d13f0
msyiham <muhammad.syiham.2105336@students.um.ac.id>
2024-05-06 11:02:04 +0700

    first commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9fcb0d70e1e05161"; }
+/******/ 		__webpack_require__.h = function() { return "ae763a63eaa83544"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
